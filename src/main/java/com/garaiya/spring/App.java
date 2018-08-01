85ef883b6c2b4f2f60883faed12d027ae14ccdee
garaiya <garaiya@aliyun.com>
2018-08-01 19:56:41 +0800

    ioc & di

---
+++ src/main/java/com/garaiya/spring/App.java
@@ -0,0 +1,18 @@
+package com.garaiya.spring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        BeanFactory beanFactory;
+        ApplicationContext applicationContext;
+    }
+}
